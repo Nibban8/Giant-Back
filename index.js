2202aaff272b44858356e5c2108cabc0e2920cb5
IO <al18760117@ite.edu.mx>
2021-04-16 09:50:50 -0700

    Configuracion inicial BD

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://soyadmin:mongotest123@cluster0.exwt9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Servidor corriendo en el puerto ${PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
