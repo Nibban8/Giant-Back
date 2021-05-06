6628c1558d75352f2e78f930bc4014fce1ec549d
IO <al18760117@ite.edu.mx>
2021-05-06 01:11:05 -0700

    Terminadas todas las rutas para "Partes"

--- routes/parts.js
+++ routes/parts.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { getParts, partCreation } from '../controllers/parts.js';
+import {
+  deletePart,
+  getPart,
+  getParts,
+  partCreation,
+  updatePart,
+} from '../controllers/parts.js';
 
 const router = express.Router();
 
 router.get('/', getParts);
 router.post('/', partCreation);
+router.put('/:id', updatePart);
+router.delete('/:id', deletePart);
+router.get('/:id', getPart);
 
 export default router;
