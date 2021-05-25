d6e841cebcd7491dab15962c3813c75d8d437294
IO <al18760117@ite.edu.mx>
2021-05-25 07:41:32 -0700

    Pago

--- index.js
+++ index.js
@@ -1,31 +1,70 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const firebase = require('firebase');
 
-import postRoutes from './routes/posts.js';
-import partRoutes from './routes/parts.js';
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use('/parts', partRoutes);
+var firebaseConfig = {
+  apiKey: 'AIzaSyCnDNExUxaOHJDkJCbU0x_KjxJCDbmPuuQ',
+  authDomain: 'giant-1d6c6.firebaseapp.com',
+  projectId: 'giant-1d6c6',
+  storageBucket: 'giant-1d6c6.appspot.com',
+  messagingSenderId: '419535352577',
+  appId: '1:419535352577:web:1a338cd78eb42e52ec2100',
+};
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
+const db = fb.firestore();
 
-const CONNECTION_URL =
-  'mongodb+srv://soyadmin:mongotest123@cluster0.exwt9.mongodb.net/GiantDB?retryWrites=true&w=majority';
+const stripe = require('stripe')(
+  'sk_test_51IujvAIqOpiH1XDDQYgrNyuA1gjsfgYvfEFnON1dr4CRphI5FwNRjVCyqvZTinPVgspbeDN3MuRhIcKW3dCNSYNq003Jt4LYy9'
+);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Servidor corriendo en el puerto ${PORT}`)
-    )
-  )
-  .catch((error) => console.log(error.message));
+app.post('/checkout', async (req, res) => {
+  const { equipo } = req.body;
 
-mongoose.set('useFindAndModify', false);
+  console.log(req.body);
+
+  let items = [];
+
+  for (const parte of equipo) {
+    const partRef = db.collection('partes').doc(parte);
+    const doc = await partRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      const { nombre, precio } = doc.data();
+      const item = {
+        price_data: {
+          currency: 'mxn',
+          product_data: {
+            name: nombre,
+          },
+          unit_amount: parseFloat(precio) * 100,
+        },
+        quantity: 1,
+      };
+      items.push(item);
+    }
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: items,
+    mode: 'payment',
+    success_url: 'https://example.com/success',
+    cancel_url: 'https://example.com/cancel',
+  });
+
+  res.json({ id: session.id });
+});
+
+app.listen(5000, () =>
+  console.log(`Aplicacion corriendo en puerto ${PORT} \n`)
+);
